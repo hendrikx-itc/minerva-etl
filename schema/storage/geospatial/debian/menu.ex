2559e29815f9c64d3fd5a46abd81208cddabb1b6
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2013-09-13 12:18:55 +0200

    Fix installation of database schema scripts

--- schema/storage/geospatial/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(minerva-storage-geospatial-schema):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="minerva-storage-geospatial-schema" command="/usr/bin/minerva-storage-geospatial-schema"
