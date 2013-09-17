c5004ae325a64422e6f15cd656ffa4800a7ca6a1
Matthijs van der Vleuten <git@zr40.nl>
2013-09-17 11:51:15 +0200

    Remove debian packaging example files

--- schema/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(minerva-database):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="minerva-database" command="/usr/bin/minerva-database"
--- schema/debian/minerva-database.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the minerva-database package
-#
-0 4	* * *	root	[ -x /usr/bin/minerva-database_maintenance ] && /usr/bin/minerva-database_maintenance
