90fef8fa186a23890d561a625e29b3a5e4e2a7ab
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2013-09-26 09:56:37 +0200

    Refactoring.

--- python-package/integration_tests/consume_jobs.py
+++ python-package/integration_tests/consume_jobs.py
@@ -1,10 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Consumes all jobs in the test database.
+"""
 from __future__ import print_function
+__docformat__ = "restructuredtext en"
+
+__copyright__ = """
+Copyright (C) 2011-2013 Hendrikx-ITC B.V.
+
+Distributed under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option) any later
+version.  The full license is in the file COPYING, distributed as part of
+this software.
+"""
 from contextlib import closing
 from time import sleep
 
 from minerva.util import each
 from minerva.test import connect
-from minerva.system.jobqueue import get_job, NoJobAvailable
+from minerva.system.jobqueue import get_job
 
 
 TIMEOUT = 1.0
@@ -21,15 +35,12 @@ def run(conn):
 
 def jobs(conn):
     while True:
-        try:
-            with closing(conn.cursor()) as cursor:
-                job = get_job(cursor)
-        except:
-            conn.rollback()
-        else:
-            conn.commit()
-            yield job
-            sleep(1.0)
+        with closing(conn.cursor()) as cursor:
+            job = get_job(cursor)
+
+        conn.commit()
+        yield job
+        sleep(TIMEOUT)
 
 
 if __name__ == "__main__":
--- python-package/integration_tests/generate_jobs.py
+++ python-package/integration_tests/generate_jobs.py
@@ -1,31 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Generates jobs in the test database.
+"""
+__docformat__ = "restructuredtext en"
+
+__copyright__ = """
+Copyright (C) 2011-2013 Hendrikx-ITC B.V.
+
+Distributed under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option) any later
+version.  The full license is in the file COPYING, distributed as part of
+this software.
+"""
 import random
 from contextlib import closing
+from functools import partial
 
+from minerva.util import each, expand_args
 from minerva.test import connect
 from minerva.system.jobqueue import enqueue_job
 from minerva.system.helpers import add_job_source, get_job_source
 
 
 def main():
+    """Script entry point."""
     with closing(connect()) as conn:
-        run(conn)
+        jobsource_id = add_dummy_jobsource(conn)
+
+        run(conn, jobsource_id)
 
 
-def run(conn):
-    jobsource_id = add_dummy_jobsource(conn)
+def run(conn, jobsource_id):
+    """The actual job generation loop."""
+    each(expand_args(partial(enqueue, conn, jobsource_id)), job_generator())
 
-    for path, size in job_generator():
-        description = '{{"uri": {}}}'.format(path)
-        enqueue_job(conn, 'dummy', description, size, jobsource_id)
-        conn.commit()
+
+def enqueue(conn, jobsource_id, description, size):
+    enqueue_job(conn, 'dummy', description, size, jobsource_id)
+    conn.commit()
 
 
 def job_generator():
     while True:
         path = "/data/kpi_1.csv"
         size = random.randint(100, 100000)
+        description = '{{"uri": {}}}'.format(path)
 
-        yield path, size
+        yield description, size
 
 
 def add_dummy_jobsource(conn):
