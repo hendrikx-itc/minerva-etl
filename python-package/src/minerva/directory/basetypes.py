7b9d63e415f17832ce677d91b7807b1869c5559c
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2013-10-01 23:46:47 +0200

    Move type specific functions into the type classes as static methods.

--- python-package/src/minerva/directory/basetypes.py
+++ python-package/src/minerva/directory/basetypes.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Provides classes for base types like EntityType and DataSource."""
 __docformat__ = "restructuredtext en"
 
 __copyright__ = """
@@ -26,6 +27,62 @@ class EntityType(object):
     def __str__(self):
         return self.name
 
+    @staticmethod
+    def create(cursor, name, description):
+        """Create new entity type and add it to the database."""
+        query = (
+            "INSERT INTO directory.entitytype (id, name, description) "
+            "VALUES (DEFAULT, %s, %s) "
+            "RETURNING *")
+
+        args = name, description
+
+        cursor.execute(query, args)
+
+        return EntityType(*cursor.fetchone())
+
+    @staticmethod
+    def get(cursor, entitytype_id):
+        """Return the entity type matching the specified Id."""
+        query = (
+            "SELECT id, name, description "
+            "FROM directory.entitytype "
+            "WHERE id = %s")
+
+        args = (entitytype_id,)
+
+        cursor.execute(query, args)
+
+        if cursor.rowcount > 0:
+            return EntityType(*cursor.fetchone())
+
+    @staticmethod
+    def get_by_name(cursor, name):
+        """Return the entitytype with name `name`."""
+        sql = (
+            "SELECT id, name, description "
+            "FROM directory.entitytype "
+            "WHERE lower(name) = lower(%s)")
+
+        args = (name,)
+
+        cursor.execute(sql, args)
+
+        if cursor.rowcount > 0:
+            return EntityType(*cursor.fetchone())
+
+    @staticmethod
+    def from_name(cursor, name):
+        """
+        Return new or existing entitytype with name `name`.
+        """
+        args = (name, )
+
+        cursor.callproc("directory.name_to_entitytype", args)
+
+        if cursor.rowcount > 0:
+            return EntityType(*cursor.fetchone())
+
 
 class Entity(object):
     """
@@ -46,6 +103,54 @@ class Entity(object):
 
     def __str__(self):
         return self.name
+
+    @staticmethod
+    def create_from_dn(cursor, dn):
+        """
+        :param conn: A cursor an a Minerva Directory database.
+        :param dn: The distinguished name of the entity.
+        """
+        cursor.callproc("directory.create_entity", (dn,))
+
+        row = cursor.fetchone()
+
+        id, _first_appearance, name, entitytype_id, dn, parent_id = row
+
+        return Entity(id, name, entitytype_id, dn, parent_id)
+
+    @staticmethod
+    def get(cursor, entity_id):
+        """Return entity with specified distinguished name."""
+        args = (entity_id,)
+
+        cursor.callproc("directory.getentitybyid", args)
+
+        if cursor.rowcount == 1:
+            (dn, entitytype_id, entity_name, parent_id) = cursor.fetchone()
+
+            return Entity(entity_id, entity_name, entitytype_id, dn, parent_id)
+
+    @staticmethod
+    def get_by_dn(cursor, dn):
+        """Return entity with specified distinguished name."""
+        args = (dn,)
+
+        cursor.callproc("directory.getentitybydn", args)
+
+        if cursor.rowcount == 1:
+            (entity_id, entitytype_id, entity_name, parent_id) = cursor.fetchone()
+
+            return Entity(entity_id, entity_name, entitytype_id, dn, parent_id)
+
+    @staticmethod
+    def from_dn(cursor, dn):
+        cursor.callproc("directory.dn_to_entity", (dn,))
+
+        row = cursor.fetchone()
+
+        id, first_appearance, name, entitytype_id, _dn, parent_id = row
+
+        return Entity(id, name, entitytype_id, dn, parent_id)
 
 
 class DataSource(object):
@@ -68,6 +173,64 @@ class DataSource(object):
         self.timezone = tzinfo.zone
 
     tzinfo = property(get_tzinfo, set_tzinfo)
+
+    @staticmethod
+    def create(cursor, name, description, timezone):
+        """
+        Create new datasource
+        :param cursor: cursor instance used to store into the Minerva database.
+        :param name: identifying name of data source.
+        :param description: A short description.
+        :param timezone: Timezone of data originating from data source.
+        """
+        query = (
+            "INSERT INTO directory.datasource "
+            "(id, name, description, timezone) "
+            "VALUES (DEFAULT, %s, %s, %s) RETURNING *")
+
+        args = name, description, timezone
+
+        cursor.execute(query, args)
+
+        return DataSource(*cursor.fetchone())
+
+    @staticmethod
+    def get(cursor, datasource_id):
+        """Return the datasource with the specified Id."""
+        query = (
+            "SELECT id, name, description, timezone "
+            "FROM directory.datasource "
+            "WHERE id=%s")
+
+        args = (datasource_id,)
+
+        cursor.execute(query, args)
+
+        if cursor.rowcount > 0:
+            return DataSource(*cursor.fetchone())
+
+    @staticmethod
+    def get_by_name(cursor, name):
+        """Return the datasource with the specified name."""
+        query = (
+            "SELECT id, name, description, timezone "
+            "FROM directory.datasource "
+            "WHERE lower(name)=lower(%s)")
+
+        args = (name,)
+
+        cursor.execute(query, args)
+
+        if cursor.rowcount > 0:
+            return DataSource(*cursor.fetchone())
+
+    @staticmethod
+    def from_name(cursor, name):
+        """Return new or existing datasource with name `name`."""
+        cursor.callproc("directory.name_to_datasource", (name,))
+
+        if cursor.rowcount > 0:
+            return DataSource(*cursor.fetchone())
 
 
 class TagGroup(object):
