8191152bf79bd625a385594adcc3444022341fee
Tommy van Schie <tommy.vanschie@hendrikx-itc.nl>
2013-11-05 16:15:11 +0100

    Updated "retrieve_attributes_for_entity" for new attribute layout.

--- python-package/src/minerva/storage/attribute/retrieve.py
+++ python-package/src/minerva/storage/attribute/retrieve.py
@@ -18,7 +18,6 @@ from operator import itemgetter
 from minerva.util.timestamp import to_unix_timestamp
 from minerva.directory import helpers
 from minerva.directory.helpers import get_entitytype_by_id
-from minerva.storage.attribute import schema
 
 
 def enquote_ident(ident):
@@ -123,7 +122,7 @@ def retrieve_related(conn, table_name, attribute_names, entities,
     where_parts = []
     join_parts = []
 
-    full_base_tbl_name = "{0}.\"{1}\"".format(schema.name, table_name)
+    full_base_tbl_name = "attribute.\"{1}\"".format(table_name)
 
     attr_columns = map(enquote_ident, attribute_names)
 
@@ -162,11 +161,10 @@ def retrieve_related(conn, table_name, attribute_names, entities,
 def retrieve_attributes_for_entity(conn, entity_id, attribute_ids):
     query = (
         "SELECT a.id, a.name, a.table_name, et.name as entitytype_name "
-        "FROM {}.attribute a "
+        "FROM attribute_directory.attribute a "
         "JOIN directory.entitytype et ON et.id = a.entitytype_id "
         "WHERE a.id IN ({}) "
-        "ORDER BY a.table_name").format(
-        schema.name, ",".join(map(str, attribute_ids)))
+        "ORDER BY a.table_name").format(",".join(map(str, attribute_ids)))
 
     entity = helpers.get_entity_by_id(conn, entity_id)
 
