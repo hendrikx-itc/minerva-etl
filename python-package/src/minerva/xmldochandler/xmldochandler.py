7b7ffa6c7e3169f36a0b43453ea180ffe0d22205
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-04-25 15:28:18 +0200

    Cleanup code style

--- python-package/src/minerva/xmldochandler/xmldochandler.py
+++ python-package/src/minerva/xmldochandler/xmldochandler.py
@@ -25,14 +25,15 @@ class XmlDocHandler(ContentHandler):
         self.onstartprefixmapping = None
 
     def startElementNS(self, name, qname, attrs):
-        matching_handler = None
-
         if len(self._stack) > 0:
             elementtype = self._stack[-1][0].elementtype
 
             matching_handler = elementtype.get_child_elementhandler(name[1])
         else:
-            matching_handler = self.schemacontext.get_elementhandler(name[0], name[1])
+            matching_handler = self.schemacontext.get_elementhandler(
+                name[0],
+                name[1]
+            )
 
         if matching_handler:
             attributes = matching_handler.elementtype.get_attributes()
@@ -60,21 +61,32 @@ class XmlDocHandler(ContentHandler):
             # Should not occur often, so performance is not an issue here.
             if len(self._stack) > 0:
                 parent_handler, context = self._stack[-1]
-                raise Exception("No handler found for element '{0}' under parent \
-element handler '{1}'".format(name[1], parent_handler.name))
+                raise Exception(
+                    "No handler found for element '{0}' under parent element "
+                    "handler '{1}'".format(name[1], parent_handler.name)
+                )
             else:
-                raise Exception("No handler found for root element '{0}'".format(name[1]))
+                raise Exception(
+                    "No handler found for root element '{0}'".format(name[1])
+                )
 
-            dummy_elementhandler = XmlElementHandler(name[1], XmlElementType(None, None))
+            dummy_elementhandler = XmlElementHandler(
+                name[1], XmlElementType(None, None)
+            )
 
             if len(self._stack) > 0:
-                self._stack[-1][0].elementtype.addchildelementhandlers(dummy_elementhandler)
+                self._stack[-1][0].elementtype.addchildelementhandlers(
+                    dummy_elementhandler
+                )
 
             self._stack.append((dummy_elementhandler, None))
 
-            logging.debug("Start of unregistered handler {0!s}\n{1:s}".format(
-                name, "->".join(
-                    (str(handler) for handler, context in self._stack))))
+            logging.debug(
+                "Start of unregistered handler {0!s}\n{1:s}".format(
+                    name,
+                    "->".join(str(handler) for handler, context in self._stack)
+                )
+            )
 
     def endElementNS(self, name, qname):
         (handler, context) = self._stack.pop()
