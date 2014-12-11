dfe6a77e18ae773dca13dd30c44c03054b2e9359
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-12-11 16:03:09 +0100

    Fix dependees test

--- schema/tests/attribute/dependees.sql
+++ schema/tests/attribute/dependees.sql
@@ -15,6 +15,7 @@ SELECT is(
     ARRAY[
         'attribute_history."test_Node_at"(pg_catalog.timestamptz)',
         'attribute_history."test_Node_at"(pg_catalog.int4, pg_catalog.timestamptz)',
+        'attribute_history.values_hash(attribute_history."test_Node")',
         'attribute_staging."test_Node_new"',
         'attribute_history."test_Node_changes"',
         'attribute_history."test_Node_run_length"'
@@ -25,8 +26,6 @@ FROM (
     FROM (
         SELECT unnest(attribute_directory.dependees(attributestore)) d
         FROM attribute_directory.attributestore
-        JOIN directory.datasource ON datasource.id = attributestore.datasource_id
-        JOIN directory.entitytype ON entitytype.id = attributestore.entitytype_id
     ) foo
 ) bar;
 
