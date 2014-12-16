a406f38f45c88a1d9567bab336011e0c93630982
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-12-16 19:29:34 +0100

    Remove client configuration set statements

--- schema/scripts/15_dimension/10_tables.sql
+++ schema/scripts/15_dimension/10_tables.sql
@@ -1,10 +1,3 @@
-SET statement_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET escape_string_warning = off;
-
 CREATE SCHEMA dimension;
 ALTER SCHEMA dimension OWNER TO minerva_admin;
 
--- schema/scripts/50_storage/trend/50_views.sql
+++ schema/scripts/50_storage/trend/50_views.sql
@@ -1,10 +1,3 @@
-SET statement_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET escape_string_warning = off;
-
 SET search_path = trend, pg_catalog;
 
 CREATE VIEW view_dependencies AS
