80400734f96c349c0af739407b9e974355e6cc1b
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-12-17 12:27:37 +0100

    Define dummy relations for geospatial schema

--- schema/scripts/40_relation/90_initialize_defaults.sql
+++ schema/scripts/40_relation/90_initialize_defaults.sql
@@ -2,3 +2,17 @@ SET search_path = relation, pg_catalog;
 
 
 INSERT INTO "type" (name) VALUES ('self');
+
+-- Dummy relations to satisfy geospatial requirements
+SELECT relation.define_reverse(
+    'HandoverRelation->Cell',
+    relation.define(
+        'Cell->HandoverRelation',
+        $$SELECT 0 as source_id, 0 as target_id WHERE false;$$
+    )
+);
+
+SELECT relation.define(
+    'real_handover',
+    $$SELECT 0 as source_id, 0 as target_id WHERE false;$$
+);
