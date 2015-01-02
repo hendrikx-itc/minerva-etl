62202649686cf59d4b380a776f2eecb08c83ede3
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-01-02 11:53:02 +0100

    Add function to drop entity-at functions

--- schema/scripts/50_storage/attribute/20_functions.sql
+++ schema/scripts/50_storage/attribute/20_functions.sql
@@ -1591,6 +1591,26 @@ $$ LANGUAGE SQL STABLE;',
 $function$ LANGUAGE SQL VOLATILE;
 
 
+CREATE OR REPLACE FUNCTION attribute_directory.drop_entity_at_func_sql(attribute_directory.attributestore)
+    RETURNS text
+AS $$
+SELECT format(
+    'DROP FUNCTION attribute_history.%I(integer, timestamp with time zone)',
+    attribute_directory.at_function_name($1)
+);
+$$ LANGUAGE sql STABLE;
+
+
+CREATE OR REPLACE FUNCTION attribute_directory.drop_entity_at_func(attribute_directory.attributestore)
+    RETURNS attribute_directory.attributestore
+AS $$
+    SELECT public.action(
+        $1,
+        attribute_directory.drop_entity_at_func_sql($1)
+    );
+$$ LANGUAGE sql VOLATILE;
+
+
 CREATE OR REPLACE FUNCTION attribute_directory.create_entity_at_func_sql(attribute_directory.attributestore)
     RETURNS text[]
 AS $function$
