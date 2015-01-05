a505b44e37f9dcd4d12e9c96e75cd6b241e02ada
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-01-05 11:52:07 +0100

    Add refactored handover relation existence logic

--- schema/scripts/50_storage/geospatial/20_functions.sql
+++ schema/scripts/50_storage/geospatial/20_functions.sql
@@ -1,3 +1,105 @@
+CREATE OR REPLACE VIEW gis.handover_relation AS
+SELECT
+    cell_ho.source_id AS cell_entity_id,
+    'OUT'::text direction,
+    ho_cell.target_id AS neighbour_entity_id,
+
+    cell_ho.source_id AS source_entity_id,
+    ho_cell.target_id AS target_entity_id,
+    cell_ho.target_id AS ho_entity_id
+FROM relation."Cell->HandoverRelation" cell_ho
+JOIN relation."HandoverRelation->Cell" ho_cell on cell_ho.target_id = ho_cell.source_id
+
+UNION ALL
+
+SELECT
+    ho_cell.target_id AS cell_entity_id,
+    'IN'::text direction,
+    cell_ho.source_id AS neighbour_entity_id,
+
+    cell_ho.source_id AS source_entity_id,
+    ho_cell.target_id AS target_entity_id,
+    cell_ho.target_id AS ho_entity_id
+FROM relation."Cell->HandoverRelation" cell_ho
+JOIN relation."HandoverRelation->Cell" ho_cell on cell_ho.target_id = ho_cell.source_id;
+
+
+CREATE OR REPLACE VIEW gis.handover_relation_existence AS
+SELECT
+    handover_relation.source_entity_id source_id,
+    handover_relation.ho_entity_id handover_id,
+    handover_relation.target_entity_id target_id, 
+    trg_et.name target_name,
+    src_et.name source_name,
+    tag.name tag_name,
+    handover_relation.direction, 
+    array_agg(
+        (existence.exists, date_part('epoch', existence.timestamp))
+    ) existence
+FROM gis.handover_relation
+JOIN directory.entity src_et ON src_et.id = handover_relation.source_entity_id
+JOIN directory.entity trg_et ON trg_et.id = handover_relation.target_entity_id
+JOIN directory.entitytaglink etl ON etl.entity_id = trg_et.id
+JOIN directory.tag ON etl.tag_id = tag.id 
+JOIN directory.taggroup etg ON etg.id = tag.taggroup_id AND etg.name = 'generation'
+JOIN relation.real_handover ON real_handover.source_id = handover_relation.ho_entity_id
+JOIN directory.existence existence ON existence.entity_id = real_handover.target_id
+GROUP BY
+    handover_relation.source_entity_id,
+    handover_relation.ho_entity_id,
+    handover_relation.target_entity_id,
+    trg_et.name,
+    src_et.name,
+    tag.name,
+    handover_relation.direction;
+
+
+CREATE TYPE gis.existence_change AS (exists boolean, unix_timestamp double precision);
+
+
+CREATE OR REPLACE FUNCTION gis.get_handovers_new(integer)
+    RETURNS TABLE(
+        source_id integer,
+        handover_id integer,
+        target_id integer,
+        target_name character varying, 
+        source_name character varying, 
+        tag_name character varying, 
+        direction text,
+        existance gis.existence_change[]
+    ) AS
+$$
+    SELECT
+        handover_relation.source_entity_id source_id,
+        handover_relation.ho_entity_id handover_id,
+        handover_relation.target_entity_id target_id, 
+        trg_et.name target_name,
+        src_et.name source_name,
+        tag.name tag_name,
+        'OUT'::text direction, 
+        array_agg(
+            (existence.exists, date_part('epoch', existence.timestamp))::gis.existence_change
+        ) existence
+    FROM gis.handover_relation
+    JOIN directory.entity src_et ON src_et.id = handover_relation.source_entity_id
+    JOIN directory.entity trg_et ON trg_et.id = handover_relation.target_entity_id
+    JOIN directory.entitytaglink etl ON etl.entity_id = trg_et.id
+    JOIN directory.tag ON etl.tag_id = tag.id 
+    JOIN directory.taggroup etg ON etg.id = tag.taggroup_id AND etg.name = 'generation'
+    JOIN relation.real_handover ON real_handover.source_id = handover_relation.ho_entity_id
+    JOIN directory.existence existence ON existence.entity_id = real_handover.target_id
+    WHERE handover_relation.cell_entity_id = $1
+    GROUP BY
+        handover_relation.source_entity_id,
+        handover_relation.ho_entity_id,
+        handover_relation.target_entity_id,
+        trg_et.name,
+        src_et.name,
+        tag.name
+
+$$ LANGUAGE sql STABLE;
+
+
 -- Function: gis.get_handovers
 
 CREATE OR REPLACE FUNCTION gis.get_handovers(integer)
