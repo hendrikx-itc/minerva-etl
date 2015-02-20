c9187cecbdb196343586378ca637d76079ff058f
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-02-20 09:59:42 +0100

    Improve sub-package imports

--- src/minerva/storage/notification/__init__.py
+++ src/minerva/storage/notification/__init__.py
@@ -9,4 +9,9 @@ the Free Software Foundation; either version 3, or (at your option) any later
 version.  The full license is in the file COPYING, distributed as part of
 this software.
 """
-from engine import NotificationEngine
+from minerva.storage.notification.notificationstore import NotificationStore, \
+    NotificationStoreDescriptor
+from minerva.storage.notification.attribute import Attribute, \
+    AttributeDescriptor
+from minerva.storage.notification.package import Package
+from minerva.storage.notification.record import Record
