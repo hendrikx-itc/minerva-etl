117915fbc717123957766859f2203f4caa64d747
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-02-25 14:15:42 +0100

    Fix entity tag integration tests

--- integration_tests/test_entitytags.py
+++ integration_tests/test_entitytags.py
@@ -1,28 +1,24 @@
 from contextlib import closing
 
-from nose.tools import eq_
-
-from minerva.test import with_conn, clear_database
+from minerva.test import with_conn, clear_database, eq_
 
 
 @with_conn(clear_database)
 def test_create_alias(conn):
     with closing(conn.cursor()) as cursor:
-        schema = 'directory'
-
         query = (
-            "INSERT INTO {0}.{1} (id, name, description) VALUES "
+            "INSERT INTO directory.entity_type (id, name, description) VALUES "
             "(1, 'test_type', 'test entity type')"
-        ).format(schema, "entitytype")
+        )
 
         cursor.execute(query)
 
         query = (
-            "INSERT INTO {0}.{1} "
-            "(id, first_appearance, \"name\", entitytype_id, dn, parent_id) "
+            "INSERT INTO directory.entity "
+            "(id, created, \"name\", entity_type_id, dn, parent_id) "
             "VALUES"
             "(DEFAULT, now(), 'test_entity', 1, 'type=12345', DEFAULT)"
-        ).format(schema, "entity")
+        )
 
         cursor.execute(query)
 
@@ -33,35 +29,41 @@ def test_create_alias(conn):
 @with_conn(clear_database)
 def test_create_tag(conn):
     with closing(conn.cursor()) as cursor:
-        schema = 'directory'
+        cursor.execute(
+            "INSERT INTO directory.entity_type (id, name, description) "
+            "VALUES (1, 'test_type', 'test entity type')"
+        )
 
-        cursor.execute("""INSERT INTO {0}.{1} (id, name, description) VALUES
-        (1, 'test_type', 'test entity type')""".format(schema, "entitytype"))
-
-        cursor.execute("""INSERT INTO {0}.{1} (id, first_appearance,
-        "name", entitytype_id, dn, parent_id) VALUES
-        (DEFAULT, now(), 'test_entity', 1, 'type=12345', DEFAULT )
-        """.format(schema, "entity"))
+        cursor.execute(
+            "INSERT INTO directory.entity ("
+            "id, created, name, entity_type_id, dn, parent_id"
+            ") VALUES ("
+            "DEFAULT, now(), 'test_entity', 1, 'type=12345', DEFAULT"
+            ")"
+        )
 
         conn.commit()
         eq_(row_count(conn, "directory", "tag"), 1)
 
 
 @with_conn(clear_database)
-def test_create_entitytaglink(conn):
+def test_create_entity_tag_link(conn):
     with closing(conn.cursor()) as cursor:
-        schema = 'directory'
+        cursor.execute(
+            "INSERT INTO directory.entity_type (id, name, description) "
+            "VALUES (1, 'test_type', 'test entity type')"
+        )
 
-        cursor.execute("""INSERT INTO {0}.{1} (id, name, description) VALUES
-        (1, 'test_type', 'test entity type')""".format(schema, "entitytype"))
-
-        cursor.execute("""INSERT INTO {0}.{1} (id, first_appearance,
-        "name", entitytype_id, dn, parent_id) VALUES
-        (DEFAULT, now(), 'test_entity', 1, 'type=12345', DEFAULT )
-        """.format(schema, "entity"))
+        cursor.execute(
+            "INSERT INTO directory.entity ("
+            "id, created, name, entity_type_id, dn, parent_id"
+            ") "
+            "VALUES (DEFAULT, now(), 'test_entity', 1, 'type=12345', DEFAULT)"
+        )
 
         conn.commit()
-        eq_(row_count(conn, "directory", "entitytaglink"), 1)
+
+        eq_(row_count(conn, "directory", "entity_tag_link"), 1)
 
 
 def row_count(conn, schema, table):
