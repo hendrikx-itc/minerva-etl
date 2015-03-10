ab438589c3df67bfef808cf22a9b5ab626f78ce4
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-03-10 14:17:37 +0100

    Fix EntityType integration tests

--- integration_tests/directory/test_entity_type.py
+++ integration_tests/directory/test_entity_type.py
@@ -8,10 +8,10 @@ from minerva.test import with_conn
 def test_get_entity_type(conn):
     with closing(conn.cursor()) as cursor:
         new_entity_type = EntityType.create(
-            cursor, "test_get_entity_type", "short description of type"
-        )
+            "test_get_entity_type", "short description of type"
+        )(cursor)
 
-        entity_type = EntityType.get_by_name(cursor, "test_get_entity_type")
+        entity_type = EntityType.get_by_name("test_get_entity_type")(cursor)
 
     assert entity_type.id == new_entity_type.id
     assert entity_type.name == "test_get_entity_type"
@@ -21,10 +21,10 @@ def test_get_entity_type(conn):
 def test_get_entity_type_by_id(conn):
     with closing(conn.cursor()) as cursor:
         new_entity_type = EntityType.create(
-            cursor, "test_get_entitytype_by_id", "short description of type"
-        )
+            "test_get_entitytype_by_id", "short description of type"
+        )(cursor)
 
-        entity_type = EntityType.get(cursor, new_entity_type.id)
+        entity_type = EntityType.get(new_entity_type.id)(cursor)
 
     assert entity_type.id == new_entity_type.id
     assert entity_type.name == "test_get_entitytype_by_id"
@@ -34,8 +34,8 @@ def test_get_entity_type_by_id(conn):
 def test_create_entity_type(conn):
     with closing(conn.cursor()) as cursor:
         entity_type = EntityType.create(
-            cursor, "test_create_entitytype", "short description of type"
-        )
+            "test_create_entitytype", "short description of type"
+        )(cursor)
 
     assert entity_type.id is not None
     assert entity_type.name == "test_create_entitytype"
@@ -44,7 +44,7 @@ def test_create_entity_type(conn):
 @with_conn()
 def test_name_to_entity_type(conn):
     with closing(conn.cursor()) as cursor:
-        entity_type = EntityType.from_name(cursor, "test_name_to_entitytype")
+        entity_type = EntityType.from_name("test_name_to_entitytype")(cursor)
 
     assert entity_type is not None
     assert entity_type.name == "test_name_to_entitytype"
