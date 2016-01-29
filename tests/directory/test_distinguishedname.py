522e2b4b9e634c99b3f543ef6c5f89c224e49977
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2016-01-29 13:16:34 +0100

    Remove source file headers

--- tests/directory/test_distinguishedname.py
+++ tests/directory/test_distinguishedname.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__docformat__ = "restructuredtext en"
-
-__copyright__ = """
-Copyright (C) 2008-2015 Hendrikx-ITC B.V.
-
-Distributed under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option) any later
-version.  The full license is in the file COPYING, distributed as part of
-this software.
-"""
 from nose.tools import assert_raises, assert_true, assert_false, assert_equal
 
 from minerva.directory.distinguishedname import explode, split_parts, escape, DistinguishedName
--- tests/directory/test_query.py
+++ tests/directory/test_query.py
@@ -2,16 +2,6 @@
 """
 Unit tests for compiling Minerva Queries to SQL.
 """
-__docformat__ = "restructuredtext en"
-
-__copyright__ = """
-Copyright (C) 2008-2015 Hendrikx-ITC B.V.
-
-Distributed under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option) any later
-version.  The full license is in the file COPYING, distributed as part of
-this software.
-"""
 from minerva.test import eq_, raises
 from minerva.directory.query import compile_sql, QueryError
 
