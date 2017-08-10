ad869bc23166c90a71c1e2ac7a0d1d46bbb0cd2d
Mike Kist <mike.kist@hendrikx-itc.nl>
2017-08-10 10:35:37 +0200

    fix pep8 violations

--- src/minerva/storage/trend/__init__.py
+++ src/minerva/storage/trend/__init__.py
@@ -1,4 +1,1 @@
 # -*- coding: utf-8 -*-
-from minerva.storage.trend.trend import TrendDescriptor, Trend
-from minerva.storage.trend.tabletrendstore import TableTrendStore
-from minerva.storage.trend.viewtrendstore import ViewTrendStore
