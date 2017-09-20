48f5586471c447c7e711eeaa4aeefda70f43a127
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2017-09-20 10:18:24 +0200

    Fix version loading in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 """Distutils install script."""
 import sys
-import minerva
 from setuptools import setup
+from distutils.util import convert_path
 
-sys.path.insert(0, "src")
+
+# Load module with version
+ns = {}
+
+mod_path = 'src/minerva/__init__.py'
+
+with open(mod_path) as mod_file:
+    exec(mod_file.read(), ns)
+
 
 setup(
     name="minerva",
     author="Hendrikx ITC",
     author_email="info@hendrikx-itc.nl",
-    version=minerva.__version__,
+    version=ns['__version__'],
     license="GPL",
     install_requires=[
         "pytz", "psycopg2>=2.2.1", "PyYAML", "configobj",
