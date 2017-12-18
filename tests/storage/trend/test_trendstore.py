96ed8bfe5e7e1252af3d03ba599ba7b04f6feb42
Andre Engels <andre.engels@hendrikx-itc.nl>
2017-12-18 13:40:40 +0100

    correcting existing unit tests

--- tests/storage/trend/test_trendstore.py
+++ tests/storage/trend/test_trendstore.py
@@ -9,23 +9,23 @@ class TestTrendStore(unittest.TestCase):
     def test_constructor(self):
         trend_store = TrendStore(
             id_=42,
-            name='test-trend-store',
             data_source=DataSource(1, 'test-source', 'description'),
             entity_type=EntityType(11, 'TestType', 'description'),
             granularity=create_granularity('1 day'),
-            trends=[]
         )
 
         self.assertIsNotNone(trend_store)
 
-    def test_base_table_name(self):
-        trend_store = TrendStore(
-            id_=42,
-            name='test-trend-store',
-            data_source=DataSource(1, 'test-source', 'description'),
-            entity_type=EntityType(11, 'TestType', 'description'),
-            granularity=create_granularity('1 day'),
-            trends=[]
-        )
-
-        self.assertEqual(trend_store.table_name(), 'test-trend-store')
+# Removed - old version
+#
+#    def test_base_table_name(self):
+#        trend_store = TrendStore(
+#            id_=42,
+#            name='test-trend-store',
+#            data_source=DataSource(1, 'test-source', 'description'),
+#            entity_type=EntityType(11, 'TestType', 'description'),
+#            granularity=create_granularity('1 day'),
+#            trends=[]
+#        )
+#
+#        self.assertEqual(trend_store.table_name(), 'test-trend-store')
